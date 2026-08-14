bb8aaba4ad8c29533d479d0245ba9d6cda26b4f4
OuterHeavenX <60826969+OuterHeavenX@users.noreply.github.com>
2026-08-14 16:02:16 -0500

    Bump cache for combat progression milestone

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'shadow-streets-v8';
+const CACHE_NAME = 'shadow-streets-v9';
 const ASSETS = [
     './',
     './index.html',
@@ -7,36 +7,6 @@ const ASSETS = [
     './css/livingBlocks.css',
     './js/main.js'
 ];
-
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(cache => cache.addAll(ASSETS))
-        .then(() => self.skipWaiting())
-    );
-});
-
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys()
-        .then(keys => Promise.all(
-            keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-        ))
-        .then(() => self.clients.claim())
-    );
-});
-
-self.addEventListener('fetch', event => {
-    if (event.request.method !== 'GET') return;
-    event.respondWith(
-        fetch(event.request)
-        .then(response => {
-            if (response && response.ok && event.request.url.startsWith(self.location.origin)) {
-                const copy = response.clone();
-                caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-            }
-            return response;
-        })
-        .catch(() => caches.match(event.request))
-    );
-});
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{if(response&&response.ok&&event.request.url.startsWith(self.location.origin)){const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy));}return response;}).catch(()=>caches.match(event.request)));});
